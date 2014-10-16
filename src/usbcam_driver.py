7611063fe300454499616c08453e2bc36526492d
Matthew Klein <ggallin08@gmail.com>
2014-10-16 13:56:00 -0400

    First stab at a usb webcam driver

---
+++ src/usbcam_driver.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import rospy
+
+from sensor_msgs.msg import Image
+from std_msgs.msg import String
+from cv_bridge import CvBridge, CvBridgeError
+
+import cv2
+import sys
+
+if __name__ == "__main__":
+  #ROS init
+  rospy.init_node('usbcam_driver')
+  imPub = rospy.Publisher('/camera/image', Image, queue_size=1)
+  
+  cap = cv2.VideoCapture(0) # Create video capture object
+  bridge = CvBridge() # Create bridge between OpenCV and ROS
+
+  while not rospy.is_shutdown():
+    cvImage = cap.read()
+    rosImageMsg = bridge.cv2_to_imgmsg(cvImage[1], encoding="passthrough")
+    rosImageMsg.header.stamp = rospy.get_rostime()
+    imPub.publish(rosImageMsg)
